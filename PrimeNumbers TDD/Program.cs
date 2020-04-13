c42277b9aad9aa8a9632aefa8a77898f374979bb
Sophia Carpenter <carpes4@uw.edu>
2020-04-13 16:02:14 -0700

    Add project files.

---
+++ PrimeNumbers TDD/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeNumbers_TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+        static void Test() 
+        {
+            Console.Write("Testing 1 passes: ");
+            Console.WriteLine(Test1());
+            
+        }
+        static bool Test1() 
+        {   
+            List<int> blankList = new List<int>();
+            return blankList.SequenceEqual(PrimeNumbers(1));
+        }
+        
+
+        static List<int> PrimeNumbers(int num) 
+        {
+            List<int> primeNumbers = new List<int>();
+            
+            return primeNumbers;
+        }
+    }
+}
